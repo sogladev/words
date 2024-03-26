e262e1a43432b0f9114ddde3a0a94c396c401eab
Jelle Meeus <jellemeeus@hotmail.com>
2024-03-26 15:31:47 +0100

    select dataset english

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
-def load_english(dataset='oxford_3000'):
-    df = pd.read_pickle(f"./data/english/{dataset}.pkl")
-    df.head()
-    data = df[["word", "type", "cefr", "phon_br", "phon_n_am", "definition", "example"]]
-    data['cefr'] = data['cefr'].map(lambda x: x.strip().upper())
-    data = data.rename(columns={'phon_br' : 'phonetics (UK)'})
-    data = data.rename(columns={'phon_n_am' : 'phonetics (US)'})
-    return data
+class EnglishData:
+    def __init__(self, dataset: str):
+        self.dataset = dataset
+
+    def load_english(self):
+        dataset = self.dataset
+        df = pd.read_pickle(f"./data/english/{dataset}.pkl")
+        df.head()
+        data = df[["word", "type", "cefr", "phon_br", "phon_n_am", "definition", "example"]]
+        data['cefr'] = data['cefr'].map(lambda x: x.strip().upper())
+        data = data.rename(columns={'phon_br' : 'phonetics (UK)'})
+        data = data.rename(columns={'phon_n_am' : 'phonetics (US)'})
+        return data
 
 # "word": word, "type": type, "english": english, "frequency_rank": frequency_rank
 def load_spanish():
